8136ad98b492489a707b9a3845351e9b4f80d125
erivaniacasanova-jpg <erivaniacasanova@gmail.com>
2026-08-20 08:37:08 -0300

    Update registration-form.tsx

--- components/registration-form.tsx
+++ components/registration-form.tsx
@@ -45,13 +45,14 @@ const BRAZILIAN_STATES = [
 ]
 
 const PLANS = {
-   VIVO: [
+  VIVO: [
     { id: "69", name: "60GB COM LIGACAO", price: 69.9, esim: true },
+    { id: "52", name: "100GB COM LIGACAO", price: 99.99, esim: true },
   ],
- TIM: [
-  { id: "56", name: "100GB COM LIGACAO", price: 69.9, esim: true },
-  { id: "154", name: "500GB SEM LIGAÇÃO", price: 189.9, esim: true },
-],
+  TIM: [
+    { id: "56", name: "100GB COM LIGACAO", price: 69.9, esim: true },
+    { id: "154", name: "500GB SEM LIGAÇÃO", price: 189.9, esim: true },
+  ],
   CLARO: [
     { id: "57", name: "80GB COM LIGACAO", price: 69.9, esim: true },
     { id: "183", name: "150GB COM LIGACAO", price: 99.9, esim: true },
